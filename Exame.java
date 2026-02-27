4ccefed93982d5e75c2a3744cf68bd622f9bf97b
Vinicius Guedes <visoriano007@gmail.com>
2026-02-27 19:52:43 -0300

    Delete Exame.java

--- Exame.java
+++
@@ -1,9 +0,0 @@
-public class Exame {
-    public Consulta consulta;
-    public String data;
-    public String descritivo;
-    void mostrar(){ 
-        System.out.println("---Exame---");
-        System.out.println("Data:"+data+"\nDescrição:"+descritivo+"\n\n");
-    }
-}
